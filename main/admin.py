e33957e52228d373e82be65d680bc7fde3d53c7c
kaidenvlr <jzxcvblessed@gmail.com>
2023-04-05 18:10:19 +0600

    registered all models in admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from main.models import Team, TaskLevel, TaskList, Task
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+@admin.register(TaskLevel)
+class TaskLevelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+@admin.register(TaskList)
+class TaskListAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'color', 'team']
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'task_list', 'task_level', 'is_done']
