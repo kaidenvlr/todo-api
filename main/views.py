d00c363285154fd5ed04a97fdfb9d150514acda4
kaidenvlr <jzxcvblessed@gmail.com>
2023-04-05 18:12:04 +0600

    viewsets for task & task-list

--- main/views.py
+++ main/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from main.models import Task, TaskList
+from main.serializers import TaskSerializer, TaskListSerializer
+
+
+class TaskViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = Task.objects.all()
+        serializer = TaskSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Task.objects.all()
+        task = get_object_or_404(queryset, pk=pk)
+        serializer = TaskSerializer(task)
+        return Response(serializer.data)
+
+
+class TaskListViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = TaskList.objects.all()
+        serializer = TaskListSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = TaskList.objects.all()
+        task_list = get_object_or_404(queryset, pk=pk)
+        serializer = TaskListSerializer(task_list)
+        return Response(serializer)
